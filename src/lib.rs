740709ec7b7595456f45c871fe118d55a482b246
cheechyuanang <angchee@live.com.my>
2024-06-03 18:46:27 +0800

    clean up test

--- src/lib.rs
+++ src/lib.rs
@@ -7,39 +7,6 @@ pub mod frida_random;
 pub mod frida_verifier;
 pub mod utils;
 
-use frida_prover_channel::{BaseProverChannel, FridaProverChannel};
-use frida_random::FridaRandom;
-use winter_crypto::hashers::Blake3_256;
-use winter_fri::{FriOptions, FriProver};
-use winter_math::fft::{self};
-use winter_math::fields::f128::BaseElement;
-use winter_math::FieldElement;
-use winter_rand_utils::rand_vector;
-
-fn build_evaluations(domain_size: usize) -> Vec<BaseElement> {
-    let mut p: Vec<BaseElement> = rand_vector(domain_size / BLOWUP_FACTOR);
-    p.resize(domain_size, BaseElement::ZERO);
-    let twiddles = fft::get_twiddles::<BaseElement>(domain_size);
-    fft::evaluate_poly(&mut p, &twiddles);
-    p
-}
-
-static BLOWUP_FACTOR: usize = 8;
-pub fn fri() {
-    let options: FriOptions = FriOptions::new(BLOWUP_FACTOR, 4, 255);
-    let domain_size = 65536;
-    let evaluations = build_evaluations(domain_size);
-
-    let mut prover = FriProver::new(options.clone());
-    let mut channel = FridaProverChannel::<
-        BaseElement,
-        Blake3_256<BaseElement>,
-        Blake3_256<BaseElement>,
-        FridaRandom<Blake3_256<BaseElement>, Blake3_256<BaseElement>, BaseElement>,
-    >::new(domain_size, 32);
-    prover.build_layers(&mut channel, evaluations);
-}
-
 #[cfg(test)]
 mod tests {
     use winter_crypto::{hashers::Blake3_256, Hasher};
@@ -48,24 +15,17 @@ mod tests {
     use winter_utils::{Deserializable, Serializable, SliceReader};
 
     use crate::{
-        fri,
-        frida_prover_channel::{BaseProverChannel, BaseProverChannelTest},
+        frida_prover_channel::BaseProverChannel,
         frida_random::{FridaRandom, FridaRandomCoin},
         frida_verifier::FridaVerifier,
         utils::{build_evaluations, build_prover_channel},
     };
 
     #[test]
-    fn test_fri() {
-        fri()
-    }
-
-    #[test]
     fn test_verify() {
         type Blake3 = Blake3_256<BaseElement>;
         pub fn verify_proof(
             proof: FriProof,
-            prover_drawn_alpha: Vec<BaseElement>,
             commitments: Vec<<Blake3 as Hasher>::Digest>,
             evaluations: &[BaseElement],
             max_degree: usize,
@@ -88,16 +48,10 @@ mod tests {
                 options.folding_factor(),
             )
             .unwrap();
-            // let mut coin = DefaultRandomCoin::<Blake3>::new(&[]);
             let mut coin = FridaRandom::<Blake3, Blake3, BaseElement>::new(&[123]);
 
             let verifier =
                 FridaVerifier::new(&mut channel, &mut coin, options.clone(), max_degree)?;
-
-            let layer_alpha = verifier.layer_alphas();
-
-            println!("prover_drawn_alpha: {:?}", prover_drawn_alpha);
-            println!("layer_alpha: {:?}", layer_alpha);
 
             let queried_evaluations = positions
                 .iter()
@@ -123,7 +77,6 @@ mod tests {
             // instantiate the prover and generate the proof
             let mut prover = FriProver::new(options.clone());
             prover.build_layers(&mut channel, evaluations.clone());
-            let prover_drawn_alpha = channel.drawn_alphas();
 
             let positions = channel.draw_query_positions();
             let proof = prover.build_proof(&positions);
@@ -133,7 +86,6 @@ mod tests {
             let max_degree = trace_length - 1;
             let result = verify_proof(
                 proof.clone(),
-                prover_drawn_alpha.clone(),
                 commitments.clone(),
                 &evaluations,
                 max_degree,
@@ -146,7 +98,6 @@ mod tests {
             // make sure proof fails for invalid degree
             let result = verify_proof(
                 proof,
-                prover_drawn_alpha,
                 commitments,
                 &evaluations,
                 max_degree - 8,
