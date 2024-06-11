b657b17e9105739a1ab8c1f02e2e802043c9d4b7
Ali <64667466+ali-rezai@users.noreply.github.com>
2024-06-11 22:30:33 +0900

    Update EigenDA benchmarking script

--- bench/eigenda/main.go
+++ bench/eigenda/main.go
@@ -12,11 +12,16 @@ import (
 	"github.com/Layr-Labs/eigenda/encoding/kzg"
 	"github.com/Layr-Labs/eigenda/encoding/kzg/prover"
 	"github.com/Layr-Labs/eigenda/encoding/kzg/verifier"
+	"github.com/Layr-Labs/eigenda/encoding/rs"
 	"github.com/Layr-Labs/eigenda/encoding/utils/codec"
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark-crypto/ecc/bn254"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
 )
 
 var Kb int = 1024
 var logs []string
+var threads int
 
 func roundUpDivide(a uint64, b uint64) uint64 {
 	return (a + b - 1) / b
@@ -50,24 +55,97 @@ func test(prover *prover.Prover, verifier *verifier.Verifier, data_size int, ope
 	data := make([]byte, data_size)
 	rand.Read(data)
 	i := runs
-	prover_sum := time.Duration(0)
-	verifier_sum := time.Duration(0)
+	convert_sum := time.Duration(0)
+	commit_sum := time.Duration(0)
+	proof_sum := time.Duration(0)
+	verify_sum := time.Duration(0)
+	commit_size := 0
+	chunk_size := 0
 	for i > 0 {
 		i--
+
+		// CONVERSION INTO FELTS
 		startTime := time.Now()
 		encodedData := codec.ConvertByPaddingEmptyByte(data)
-		blobSize := uint64(len(encodedData) / 32)
-		chunkLength, numChunks := calcChunkData(blobSize, uint64(operator_count), 100, 50)
-		commitments, frames, err := prover.EncodeAndProve(encodedData, encoding.EncodingParams{
-			ChunkLength: chunkLength,
-			NumChunks:   numChunks,
-		})
+		inputFr, err := rs.ToFrArray(encodedData)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		prover_sum += time.Since(startTime)
+		convert_sum += time.Since(startTime)
 
+		// preparation
+		blobSize := uint64(len(encodedData) / 32)
+		chunkLength, numChunks := calcChunkData(blobSize, uint64(operator_count), 100, 50)
+		params := encoding.EncodingParams{
+			ChunkLength: chunkLength,
+			NumChunks:   numChunks,
+		}
+		enc, err := prover.GetKzgEncoder(params)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// COMMITMENT GENERATION
+		startTime = time.Now()
+		polyCoeffs := inputFr
+		commit, err := enc.Commit(polyCoeffs)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		config := ecc.MultiExpConfig{}
+		var lengthCommitment bn254.G2Affine
+		_, err = lengthCommitment.MultiExp(enc.Srs.G2[:len(polyCoeffs)], polyCoeffs, config)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		shiftedSecret := enc.G2Trailing[enc.KzgConfig.SRSNumberToLoad-uint64(len(inputFr)):]
+		var lengthProof bn254.G2Affine
+		_, err = lengthProof.MultiExp(shiftedSecret, polyCoeffs, config)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		commitments := encoding.BlobCommitments{
+			Commitment:       (*encoding.G1Commitment)(&commit),
+			LengthCommitment: (*encoding.G2Commitment)(&lengthCommitment),
+			LengthProof:      (*encoding.G2Commitment)(&lengthProof),
+			Length:           uint(len(polyCoeffs)),
+		}
+		commit_sum += time.Since(startTime)
+
+		// FRAMING AND PROOF GENERATION
+		startTime = time.Now()
+		polyEvals, _, err := enc.ExtendPolyEval(polyCoeffs)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		frames, indices, err := enc.MakeFrames(polyEvals)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		paddedCoeffs := make([]fr.Element, enc.NumEvaluations())
+		copy(paddedCoeffs, polyCoeffs)
+		proofs, err := enc.ProveAllCosetThreads(paddedCoeffs, enc.NumChunks, enc.ChunkLength, enc.NumWorker)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		kzgFrames := make([]*encoding.Frame, len(frames))
+		for i, index := range indices {
+			kzgFrames[i] = &encoding.Frame{
+				Proof:  proofs[index],
+				Coeffs: frames[i].Coeffs,
+			}
+		}
+		proof_sum += time.Since(startTime)
+
+		// VERIFICATION OF A SINGLE FRAME
 		startTime = time.Now()
 		err = verifier.VerifyBlobLength(commitments)
 		if err != nil {
@@ -79,9 +157,8 @@ func test(prover *prover.Prover, verifier *verifier.Verifier, data_size int, ope
 			fmt.Println(err)
 			os.Exit(1)
 		}
-
 		samples := make([]encoding.Sample, 1)
-		for ind, frame := range frames[:1] {
+		for ind, frame := range kzgFrames[:1] {
 			samples[ind] = encoding.Sample{
 				Commitment:      commitments.Commitment,
 				Chunk:           frame,
@@ -93,7 +170,6 @@ func test(prover *prover.Prover, verifier *verifier.Verifier, data_size int, ope
 			Samples:  samples,
 			NumBlobs: 1,
 		}
-
 		err = verifier.UniversalVerifySubBatch(encoding.EncodingParams{
 			ChunkLength: chunkLength,
 			NumChunks:   numChunks,
@@ -102,17 +178,30 @@ func test(prover *prover.Prover, verifier *verifier.Verifier, data_size int, ope
 			fmt.Println(err)
 			os.Exit(1)
 		}
-
-		verifier_sum += time.Since(startTime)
+		verify_sum += time.Since(startTime)
 		fmt.Println()
+
+		commit_size = 0
+		bytes, _ := commitments.Commitment.Serialize()
+		commit_size += len(bytes)
+		bytes, _ = commitments.LengthCommitment.Serialize()
+		commit_size += len(bytes)
+		bytes, _ = commitments.LengthProof.Serialize()
+		commit_size += len(bytes)
+		// +4 for length
+		commit_size += 4
+
+		chunk_size = 0
+		chunk_size += len(kzgFrames[0].Coeffs) * 32
+		chunk_size += len(kzgFrames[0].Proof.Bytes())
 	}
 
-	logs = append(logs, fmt.Sprintf("Operators: %d, Size: %dKb, Runs: %d, Proving AVG: %v, Verification AVG: %v", operator_count, data_size/Kb, runs, prover_sum/time.Duration(runs), verifier_sum/time.Duration(runs)))
+	logs = append(logs, fmt.Sprintf("%d, %d, %dKb, %v, %v, %v, %v, %d, %d", threads, operator_count, data_size/Kb, convert_sum/time.Duration(runs), commit_sum/time.Duration(runs), proof_sum/time.Duration(runs), verify_sum/time.Duration(runs), commit_size, chunk_size))
 }
 
 func main() {
-
-	threads, err := strconv.Atoi(os.Args[1])
+	var err error
+	threads, err = strconv.Atoi(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -162,6 +251,7 @@ func main() {
 		}
 	}
 
+	fmt.Println("Threads, Operators, Data Size, Conversion, Commitment, Proofs, Verification, Commitment Size, Chunk Size")
 	for _, l := range logs {
 		fmt.Println(l)
 	}
