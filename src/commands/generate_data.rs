a70d1c269c72901d4b8fd57912470b93f5964b41
Wonjae Choi <choi@wonj.me>
2024-06-17 09:26:15 +0900

    feat: Add a test for generate_data

--- src/commands/generate_data.rs
+++ src/commands/generate_data.rs
@@ -8,3 +8,19 @@ pub fn run(size: usize, file_path: &str) {
     file.write_all(&data).expect("Unable to write data");
     println!("Generated data of size {} and saved to {}", size, file_path);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+
+    #[test]
+    fn test_generate_data() {
+        let size = 200;
+        let file_path = "data/data.bin";
+        run(size, file_path);
+        let metadata = fs::metadata(file_path).expect("Unable to read metadata");
+        assert!(metadata.is_file());
+        assert_eq!(metadata.len(), size as u64);
+    }
+}
