ca7064acefc9a43afd9debffec4786d8f4a23961
Wonjae Choi <choi@wonj.me>
2024-06-17 09:25:50 +0900

    feat: Add generate_data

--- src/commands/generate_data.rs
+++ src/commands/generate_data.rs
@@ -1,1 +1,10 @@
-pub fn run(size: usize, file_path: &str) {}
+use std::fs::File;
+use std::io::Write;
+use winter_rand_utils::rand_vector;
+
+pub fn run(size: usize, file_path: &str) {
+    let data = rand_vector::<u8>(size);
+    let mut file = File::create(file_path).expect("Unable to create file");
+    file.write_all(&data).expect("Unable to write data");
+    println!("Generated data of size {} and saved to {}", size, file_path);
+}
