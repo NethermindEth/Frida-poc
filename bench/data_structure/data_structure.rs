e49b685a368f4d6c1a20f0f43408999bd4ff6cae
Emils <AC4918@student.jamk.fi>
2024-07-29 13:55:54 +0300

    Minor code restructure

--- bench/data_structure/data_structure.rs
+++ bench/data_structure/data_structure.rs
@@ -2,57 +2,47 @@ use winter_math::{FieldElement, fields::{f64, f128}};
 use winter_rand_utils::rand_vector;
 
 pub struct DataDesign {
-    // for now, the only field needed is size..
-    pub data_size: usize
+    pub chunk_amount: usize
 }
 
 impl DataDesign {
-    pub fn new(data_size: usize) -> DataDesign {
+    pub fn new(chunk_amount: usize) -> DataDesign {
         DataDesign {
-            data_size
+            chunk_amount
         }
     }
 
+    fn data_size<E: FieldElement>(chunk_amount: usize) -> usize {
+        chunk_amount * E::ELEMENT_BYTES - (mem::size_of::<u64>() + E::ELEMENT_BYTES - 1)
+    }
 
     // Approach 1 data creation:
-    pub fn create_square_data(&self) -> Vec<Vec<u8>> {
-        let square_root_size = (self.data_size as f64).sqrt().ceil() as usize;
-
-        // Another way to create a 2D matrix (just decided to save):
-        // let data = Array::<u8, _>::random((squared_size, squared_size), Standard);
+    pub fn create_data(&self) -> Vec<Vec<u8>> {
+        let square_k = (self.chunk_amount as f64).sqrt().ceil() as usize;
+        let data_size = data_size(square_k);
     
-        let mut res = Vec::with_capacity(square_root_size as usize);
-        for _ in 0..square_root_size {
-            res.push(rand_vector::<u8>(square_root_size as usize));
+        let mut res = Vec::with_capacity(square_k);
+
+        for _ in 0..square_k {
+            res.push(rand_vector::<u8>(data_size));
         }
         res
     }
 
     // Approach 2 data creation:
     pub fn create_subsquare_data(&self) -> Vec<Vec<Vec<u8>>> {
-        let cubic_root_size = f64::powf(self.data_size as f64, 1.0 / 3.0).ceil() as u8;
+        let cubic_k = f64::powf(self.chunk_amount as f64, 1.0 / 3.0).ceil() as usize;
+        let data_size = data_size(square_k);
+
+        let mut res = Vec::with_capacity(cubic_k);
     
-        let mut res = Vec::with_capacity(cubic_root_size as usize);
-    
-        for _ in 0..cubic_root_size {
-            let mut app_res = Vec::with_capacity(cubic_root_size as usize);
-            for _ in 0..cubic_root_size {
-                app_res.push(rand_vector::<u8>(cubic_root_size as usize));
+        for _ in 0..cubic_k {
+            let mut app_res = Vec::with_capacity(cubic_k);
+            for _ in 0..cubic_k {
+                app_res.push(rand_vector::<u8>(data_size));
             }
             res.push(app_res);
         }
         res
     }
 }
-
-// fn main() {
-//     let datas = DataDesign {
-//         batch_size: 2,
-//         data_sizes: 114680,
-//         element_bytes: 8
-//     };
-
-//     let batch_size = datas.create_data();
-
-//     DataDesign::new(batch_size, data_size, element_bytes)
-// }
--- bench/data_structure/main.rs
+++ bench/data_structure/main.rs
@@ -4,13 +4,14 @@ use std::{
     time::{Duration, Instant},
 };
 
+use core::mem;
+
 use frida_poc::{
     frida_prover::{
         bench::{COMMIT_TIME, ERASURE_TIME},
         Commitment, FridaProverBuilder,
     },
-    frida_random::{FridaRandom, FridaRandomCoin},
-    frida_verifier::{das::FridaDasVerifier, traits::BaseFridaVerifier},
+    frida_verifier::das::FridaDasVerifier,
 };
 use winter_crypto::{hashers::Blake3_256, ElementHasher};
 use winter_fri::FriOptions;
@@ -19,17 +20,24 @@ use winter_rand_utils::rand_vector;
 
 mod data_structure;
 
+
 const RUNS: u32 = 10;
 
-fn data_sizes<E: FieldElement>() -> Vec<usize> {
-    // for now using more than 1 size at once results in the index out of range error, will fix it asap
+fn amount_of_chunks() -> Vec<usize> {
     vec![
-        (128 * 1024) / E::ELEMENT_BYTES * (E::ELEMENT_BYTES - 1) - 8, // 114680
-        // (256 * 1024) / E::ELEMENT_BYTES * (E::ELEMENT_BYTES - 1) - 8,
-        // (512 * 1024) / E::ELEMENT_BYTES * (E::ELEMENT_BYTES - 1) - 8,
-        // (1024 * 1024) / E::ELEMENT_BYTES * (E::ELEMENT_BYTES - 1) - 8,
-        // (2048 * 1024) / E::ELEMENT_BYTES * (E::ELEMENT_BYTES - 1) - 8,
+        16,
+        64, 
+        256,
+        1024,
+        4096,
+        16384,
+        65536
     ]
+}
+
+// move to data struct
+fn data_size<E: FieldElement>(chunk_amount: usize) -> usize {
+    chunk_amount * E::ELEMENT_BYTES - (mem::size_of::<u64>() + E::ELEMENT_BYTES - 1)
 }
 
 fn prepare_prover_builder<E: FieldElement, H: ElementHasher<BaseField = E::BaseField>>(
@@ -46,160 +54,29 @@ fn prepare_verifier<E: FieldElement, H: ElementHasher<BaseField = E::BaseField>>
     folding_factor: usize,
     remainder_max_degree: usize,
     com: Commitment<H>,
-) -> FridaDasVerifier<E, H, H, FridaRandom<H, H, E>> {
+) -> FridaDasVerifier<E, H, H> {
     let options = FriOptions::new(blowup_factor, folding_factor, remainder_max_degree);
-    let mut coin = FridaRandom::<H, H, E>::new(&[123]);
-    FridaDasVerifier::new(com, &mut coin, options.clone()).unwrap()
+    FridaDasVerifier::new(com, options.clone()).unwrap().0
 }
 
-fn run<E: FieldElement, H: ElementHasher<BaseField = E::BaseField>>() {
-    let datas = data_sizes::<E>()
+fn run_approach_1<E: FieldElement, H: ElementHasher<BaseField = E::BaseField>>() {
+    // data size
+    let mut num_chunks: usize = 0; // k^1/2
+
+    let datas = amount_of_chunks()
         .into_iter()
-        .map(|size| rand_vector::<u8>(size))
-        .collect::<Vec<_>>();
-    let num_queries = vec![8, 16, 32];
+        .map(|chunk_amount| {
+            num_chunks = data_size::<E>(chunk_amount);
 
-    let prover_options = vec![
-        (2, 2, 0),
-        (2, 2, 256),
-        (2, 4, 2),
-        (2, 4, 256),
-        (2, 8, 4),
-        (2, 8, 256),
-        (2, 16, 8),
-        (2, 16, 256),
-    ];
+            let data_struct = data_structure::DataDesign::new(num_chunks);
+            let data = data_struct.create_data();
 
-    println!("FriOptions, Queries, Data Size, Erasure Coding, Commitment, Proofs (1, 16, 32), Verification (Com, 1, 16, 32), Commitment Size, Proof Size (1, 16, 32)");
-
-    let mut results = vec![];
-
-    for opt in prover_options {
-        let prover_builder = prepare_prover_builder::<E, H>(opt.0, opt.1, opt.2);
-
-        for data in datas.iter() {
-            for num_query in num_queries.iter() {
-                let mut prove_time = (
-                    Duration::default(),
-                    Duration::default(),
-                    Duration::default(),
-                );
-                let mut verify_time = (
-                    Duration::default(),
-                    Duration::default(),
-                    Duration::default(),
-                    Duration::default(),
-                );
-
-                let mut commit_size = 0;
-                let mut proof_size = (0, 0, 0);
-
-                for _ in 0..RUNS {
-                    let (com, prover) =
-                        prover_builder.commit(&data, *num_query).unwrap();
-                    // +1 roots len, +1 batch_size, +1 num_query = +3 at the end
-                    commit_size += com.proof.size() + com.roots.len() * 32 + 3;
-
-                    let positions = rand_vector::<u64>(32)
-                        .into_iter()
-                        .map(|v| (v as usize) % com.domain_size)
-                        .collect::<Vec<_>>();
-
-                    let evaluations = positions
-                        .iter()
-                        .map(|pos| {
-                            prover.get_first_layer_evalutaions()[(pos % (com.domain_size / opt.1))
-                                * opt.1
-                                + (pos / (com.domain_size / opt.1))]
-                        })
-                        .collect::<Vec<_>>();
-
-                    let mut timer = Instant::now();
-                    let proof_0 = prover.open(&positions[0..1]);
-                    proof_size.0 += proof_0.size();
-                    prove_time.0 += timer.elapsed();
-
-                    timer = Instant::now();
-                    let proof_1 = prover.open(&positions[0..16]);
-                    proof_size.1 += proof_1.size();
-                    prove_time.1 += timer.elapsed();
-
-                    timer = Instant::now();
-                    let proof_2 = prover.open(&positions);
-                    proof_size.2 += proof_2.size();
-                    prove_time.2 += timer.elapsed();
-
-                    timer = Instant::now();
-                    let verifier = prepare_verifier::<E, H>(opt.0, opt.1, opt.2, com);
-                    verify_time.0 += timer.elapsed();
-
-                    timer = Instant::now();
-                    verifier
-                        .verify(proof_0, &evaluations[0..1], &positions[0..1])
-                        .unwrap();
-                    verify_time.1 += timer.elapsed();
-
-                    timer = Instant::now();
-                    verifier
-                        .verify(proof_1, &evaluations[0..16], &positions[0..16])
-                        .unwrap();
-                    verify_time.2 += timer.elapsed();
-
-                    timer = Instant::now();
-                    verifier.verify(proof_2, &evaluations, &positions).unwrap();
-                    verify_time.3 += timer.elapsed();
-                }
-                results.push(format!(
-                    "{:?}, {}, {}Kb, {:?}, {:?}, ({:?}, {:?}, {:?}), ({:?}, {:?}, {:?}, {:?}), {}, ({}, {}, {})",
-                    opt,
-                    num_query,
-                    data.len() / 1024,
-                    unsafe { ERASURE_TIME.unwrap() / RUNS },
-                    unsafe { COMMIT_TIME.unwrap() / RUNS },
-                    prove_time.0 / RUNS,
-                    prove_time.1 / RUNS,
-                    prove_time.2 / RUNS,
-                    verify_time.0 / RUNS,
-                    verify_time.1 / RUNS,
-                    verify_time.2 / RUNS,
-                    verify_time.3 / RUNS,
-                    commit_size / RUNS as usize,
-                    proof_size.0 / RUNS as usize,
-                    proof_size.1 / RUNS as usize,
-                    proof_size.2 / RUNS as usize,
-                ));
-
-                unsafe {
-                    ERASURE_TIME = None;
-                    COMMIT_TIME = None;
-                }
-            }
-        }
-    }
-
-    for result in results {
-        println!("{}", result);
-    }
-}
-
-fn run_batched<E: FieldElement, H: ElementHasher<BaseField = E::BaseField>>() {
-    let mut batch_size: usize = 0;
-
-    let datas = data_sizes::<E>()
-        .into_iter()
-        .map(|size| {
-            let data_struct = data_structure::DataDesign::new(size);
-            let data = data_struct.create_square_data();
-
-            batch_size = data.len();
-            println!("Batch size: {}, data size: {}", batch_size, size);
+            println!("\nk^1/2 = {}\n", num_chunks);
             data
         })
         .collect::<Vec<_>>();
     let num_queries = vec![8, 16, 32];
 
-    // when using remainder_max_degree (option 3) that's more than 16, the program results in "NotEnoughDataPoints" error.
-    // so, i have just commented out the ones that are more than 16 for testing purposes
     let prover_options = vec![
         (2, 2, 0),
         // (2, 2, 256),
@@ -211,7 +88,7 @@ fn run_batched<E: FieldElement, H: ElementHasher<BaseField = E::BaseField>>() {
         // (2, 16, 256),
     ];
 
-    println!("FriOptions, Queries, Data Size (Batched {}), Erasure Coding, Commitment, Proofs (1, 16, 32), Verification (Com, 1, 16, 32), Commitment Size, Proof Size (1, 16, 32)", batch_size);
+    println!("FriOptions, Queries, Data Size (Batched {}), Erasure Coding, Commitment, Proofs (1, 16, 32), Verification (Com, 1, 16, 32), Commitment Size, Proof Size (1, 16, 32)", num_chunks);
 
     let mut results = vec![];
 
@@ -239,7 +116,7 @@ fn run_batched<E: FieldElement, H: ElementHasher<BaseField = E::BaseField>>() {
                     let (com, prover) =
                         prover_builder.commit_batch(&data, *num_query).unwrap();
 
-                    // +1 roots len, +1 batch_size, +1 num_query = +3 at the end
+                    // +1 roots len, +1 num_chunks, +1 num_query = +3 at the end
                     commit_size += com.proof.size() + com.roots.len() * 32 + 3;
 
                     let positions = rand_vector::<u64>(32)
@@ -250,9 +127,9 @@ fn run_batched<E: FieldElement, H: ElementHasher<BaseField = E::BaseField>>() {
                     let mut evaluations = vec![];
                     for position in positions.iter() {
                         let bucket = position % (com.domain_size / opt.1);
-                        let start_index = bucket * (batch_size * opt.1)
-                            + (position / (com.domain_size / opt.1)) * batch_size;
-                        prover.get_first_layer_evalutaions()[start_index..start_index + batch_size]
+                        let start_index = bucket * (num_chunks * opt.1)
+                            + (position / (com.domain_size / opt.1)) * num_chunks;
+                        prover.get_first_layer_evalutaions()[start_index..start_index + num_chunks]
                             .iter()
                             .for_each(|e| {
                                 evaluations.push(*e);
@@ -280,25 +157,25 @@ fn run_batched<E: FieldElement, H: ElementHasher<BaseField = E::BaseField>>() {
 
                     timer = Instant::now();
                     verifier
-                        .verify(proof_0, &evaluations[0..batch_size], &positions[0..1])
+                        .verify(&proof_0, &evaluations[0..num_chunks], &positions[0..1])
                         .unwrap();
                     verify_time.1 += timer.elapsed();
 
                     timer = Instant::now();
                     verifier
-                        .verify(proof_1, &evaluations[0..batch_size * 16], &positions[0..16])
+                        .verify(&proof_1, &evaluations[0..num_chunks * 16], &positions[0..16])
                         .unwrap();
                     verify_time.2 += timer.elapsed();
 
                     timer = Instant::now();
-                    verifier.verify(proof_2, &evaluations, &positions).unwrap();
+                    verifier.verify(&proof_2, &evaluations, &positions).unwrap();
                     verify_time.3 += timer.elapsed();
                 }
                 results.push(format!(
                     "{:?}, {}, {}Kb, {:?}, {:?}, ({:?}, {:?}, {:?}), ({:?}, {:?}, {:?}, {:?}), {}, ({}, {}, {})",
                     opt,
                     num_query,
-                    data[0].len() / 1024 * batch_size,
+                    data[0].len() / 1024 * num_chunks,
                     unsafe { ERASURE_TIME.unwrap() / RUNS },
                     unsafe { COMMIT_TIME.unwrap() / RUNS },
                     prove_time.0 / RUNS,
@@ -327,24 +204,15 @@ fn run_batched<E: FieldElement, H: ElementHasher<BaseField = E::BaseField>>() {
     }
 }
 
+fn run_approach_2<E: FieldElement, H: ElementHasher<BaseField = E::BaseField>>() {
+    // Approach 2 here
+}
+
 fn main() {
-    // println!("FRI...\n\n");
-    // println!("64bit...");
-    // run::<f64::BaseElement, Blake3_256<f64::BaseElement>>();
-
-    // println!("\n128bit...");
-    // run::<f128::BaseElement, Blake3_256<f128::BaseElement>>();
-
     println!("\nBatched FRI...\n\n");
     println!("64bit...");
     run_batched::<f64::BaseElement, Blake3_256<f64::BaseElement>>();
-    // for i in [2, 4, 8, 16] {
-    //     run_batched::<f64::BaseElement, Blake3_256<f64::BaseElement>>(i);
-    // }
 
     println!("\n128bit...");
     run_batched::<f128::BaseElement, Blake3_256<f128::BaseElement>>();
-    // for i in [2, 4, 8, 16] {
-    //     run_batched::<f128::BaseElement, Blake3_256<f128::BaseElement>>(i);
-    // }
 }
