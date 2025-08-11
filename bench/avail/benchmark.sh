5b8395e45ffa841cc77c32ed6e7d9b8af2e4f846
dkgoutham <d.goutham@iitg.ac.in>
2025-08-11 14:16:31 +0530

    removed avail and eigenda folders from bench

--- bench/avail/benchmark.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-cd "$(dirname "$0")/../.."
-mkdir -p bench/avail/logs
-
-echo "Running..."
-cd "bench/avail"
-cargo build --release &> /dev/null
-./target/release/benchmark > logs/log.txt
--- bench/eigenda/benchmark.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p ./logs
-bash setup.sh
-go build -o eigen_benchmark
-for threads in 1 2 4 8; do
-    echo "Running... Threads: $threads";
-    ./eigen_benchmark $threads &> ./logs/threads_$threads.txt;
-done
